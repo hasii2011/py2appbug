a7f987f84280c0c4c708e36dbb46ee084f69316b
Humberto Sanchez II <humberto.a.sanchez.ii@gmail.com>
2025-01-21 12:54:15 -0700

    <>[]: <Put in work around>
    []
    
    []

--- py2appbug/py2appbug.py
+++ py2appbug/py2appbug.py
@@ -5,6 +5,14 @@ from PIL.Image import Image
 from PIL.Image import open as pilOpen
 from PIL.ImageFile import ImageFile
 
+#
+# I am manually importing this plugin;  When this code gets packaged by
+# py2app the plugin is NOT loaded and the code in py
+#
+# Uncomment out the import and repackage;   will now correctlly work
+# noinspection PyUnresolvedReferences
+# from PIL import PdfImagePlugin
+
 
 def demoBug():
     print('Demo')
